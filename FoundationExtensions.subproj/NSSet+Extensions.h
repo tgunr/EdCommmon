6e2c0d2bc18ad852e1d7e3c8852dce9e998aa3d8
erik <erik@ebea66e6-cd41-dd11-acd5-0211d85b5083>
2001-03-11 03:06:58 +0000

    Some extensions to conveniently create new sets coprising objects from an existing immutable set and another set or array.

---
+++ FoundationExtensions.subproj/NSSet+Extensions.h
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------------------
+//  NSSet+Extensions.h created by erik on Sat 10-Mar-2001
+//  $Id: NSSet+Extensions.h,v 1.1 2001-03-11 03:06:58 erik Exp $
+//
+//  Copyright (c) 2000 by Erik Doernenburg. All rights reserved.
+//
+//  Permission to use, copy, modify and distribute this software and its documentation
+//  is hereby granted, provided that both the copyright notice and this permission
+//  notice appear in all copies of the software, derivative works or modified versions,
+//  and any portions thereof, and that both notices appear in supporting documentation,
+//  and that credit is given to Erik Doernenburg in all documents and publicity
+//  pertaining to direct or indirect use of this code or its derivatives.
+//
+//  THIS IS EXPERIMENTAL SOFTWARE AND IT IS KNOWN TO HAVE BUGS, SOME OF WHICH MAY HAVE
+//  SERIOUS CONSEQUENCES. THE COPYRIGHT HOLDER ALLOWS FREE USE OF THIS SOFTWARE IN ITS
+//  "AS IS" CONDITION. THE COPYRIGHT HOLDER DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY
+//  DAMAGES WHATSOEVER RESULTING DIRECTLY OR INDIRECTLY FROM THE USE OF THIS SOFTWARE
+//  OR OF ANY DERIVATIVE WORK.
+//---------------------------------------------------------------------------------------
+
+#import <Foundation/NSScanner.h>
+
+@interface NSSet(EDExtensions)
+
+- (NSSet *)setByAddingObjectsFromSet:(NSSet *)otherSet;
+- (NSSet *)setByAddingObjectsFromArray:(NSArray *)anArray;
+
+@end
